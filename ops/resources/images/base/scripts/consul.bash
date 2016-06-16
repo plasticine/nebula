c4480987e9669a0af0544186a25823d410085606
Justin Morris <desk@pixelbloom.com>
2016-06-16 15:17:53 +1000

    reorganising images.

--- ops/resources/images/base/scripts/consul.bash
+++ ops/resources/images/base/scripts/consul.bash
@@ -9,21 +9,17 @@ sudo adduser --disabled-password --gecos '' consul
 sudo mkdir /var/consul
 sudo chown consul:consul /var/consul
 
+sudo mkdir -p /opt/consul
+sudo mkdir -p /opt/consul/data
+
 cd /tmp
 wget https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip -O consul.zip
 unzip consul.zip >/dev/null
 chmod +x consul
 sudo mv consul /usr/local/bin/consul
 
-sudo mkdir -p /opt/consul
-sudo mkdir -p /opt/consul/data
-
 wget https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_web_ui.zip -O consul_ui.zip
 sudo unzip consul_ui.zip -d /opt/consul/ui >/dev/null
-
-# Install consul-join
-sudo mv /tmp/provision/consul-join-cluster /usr/local/bin/consul-join-cluster
-sudo chmod +x /usr/local/bin/consul-join-cluster
 
 # Install service
 sudo mkdir -p /etc/systemd/system/consul.d
@@ -37,3 +33,7 @@ sudo systemctl daemon-reload
 sudo mv /tmp/provision/consul.rsyslog /etc/rsyslog.d/10-consul.conf
 sudo mv /tmp/provision/consul.logrotate /etc/logrotate.d/consul
 sudo service rsyslog restart
+
+# Install consul-join
+sudo mv /tmp/provision/consul-join-cluster /usr/local/bin/consul-join-cluster
+sudo chmod +x /usr/local/bin/consul-join-cluster
---
+++ ops/resources/images/base/scripts/dnsmasq.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=' '
+
+apt-get install -y dnsmasq
+
+mkdir -p /etc/dnsmasq.d
+cat > /etc/dnsmasq.d/10-consul <<'EOF'
+server=/consul/127.0.0.1#8600
+EOF
+
+systemctl enable dnsmasq
+systemctl start dnsmasq
