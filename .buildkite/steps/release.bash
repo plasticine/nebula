9e8ee0a0477b6dd0d9c095886254d6013cbe82d6
Justin Morris <desk@pixelbloom.com>
2016-08-15 00:19:58 +1000

    readonly -> export

--- .buildkite/steps/release.bash
+++ .buildkite/steps/release.bash
@@ -3,13 +3,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-readonly RELEASE_BUILD_TAG="$(echo $BUILDKITE_COMMIT | head -c 8)"
+export RELEASE_BUILD_TAG="$(echo $BUILDKITE_COMMIT | head -c 8)"
+export NOMAD_VERSION="$NOMAD_VERSION"
+export MIX_ENV="$MIX_ENV"
 
 main() {
-  export RELEASE_BUILD_TAG="$RELEASE_BUILD_TAG"
-  export NOMAD_VERSION="$NOMAD_VERSION"
-  export MIX_ENV="$MIX_ENV"
-
   services/nebula/script/build
   services/nebula/script/release
   services/web/script/build
