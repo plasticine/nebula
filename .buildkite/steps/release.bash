50eebf808bbc13d37024c3e1ee56ee8553223447
Justin Morris <desk@pixelbloom.com>
2016-08-15 22:12:27 +1000

    refactored build scripts a bit, added registry push.

--- .buildkite/steps/release.bash
+++ .buildkite/steps/release.bash
@@ -3,14 +3,15 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-export RELEASE_BUILD_TAG="$(echo $BUILDKITE_COMMIT | head -c 8)"
+readonly RELEASE_BUILD_VERSION="$(echo $BUILDKITE_COMMIT | head -c 8)"
+
 export NOMAD_VERSION="$NOMAD_VERSION"
 export MIX_ENV="$MIX_ENV"
 
 main() {
   pushd "services/nebula" > /dev/null
   echo '--- :elixir: :docker: Building app image...'
-  script/build
+  script/build "nabula_app:${RELEASE_BUILD_VERSION}"
 
   echo '--- :elixir: :truck: Making release artifact for app...'
   script/release
@@ -18,8 +19,14 @@ main() {
 
   pushd "services/web" > /dev/null
   echo '--- :nginx: :docker: Building web image...'
-  script/build
+  script/build "nabula_web:${RELEASE_BUILD_VERSION}"
   popd > /dev/null
+
+  echo '--- :crystal_ball: :docker: Pushing built images...'
+  docker tag "nabula_web:${RELEASE_BUILD_VERSION}" "${REGISTRY_PATH}/nabula_web:${RELEASE_BUILD_VERSION}"
+  docker tag "nabula_app:${RELEASE_BUILD_VERSION}" "${REGISTRY_PATH}/nabula_app:${RELEASE_BUILD_VERSION}"
+  docker push "${REGISTRY_PATH}/nabula_web:${RELEASE_BUILD_VERSION}"
+  docker push "${REGISTRY_PATH}/nabula_app:${RELEASE_BUILD_VERSION}"
 }
 
 main
