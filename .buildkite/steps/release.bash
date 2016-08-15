d9790697f84acabba6143047fc90c6b852202f2b
Justin Morris <desk@pixelbloom.com>
2016-08-15 21:53:00 +1000

    Fix output

--- .buildkite/steps/release.bash
+++ .buildkite/steps/release.bash
@@ -8,13 +8,13 @@ export NOMAD_VERSION="$NOMAD_VERSION"
 export MIX_ENV="$MIX_ENV"
 
 main() {
-  echo '+++ :elixir: Building release...'
-
+  echo '--- :elixir: Building app service...'
   pushd "services/nebula" > /dev/null
   script/build
   script/release
   popd > /dev/null
 
+  echo '--- :nginx: Building web service release...'
   pushd "services/web" > /dev/null
   script/build
   popd > /dev/null
