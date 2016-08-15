7f4b0ef0f597e6f53dba68380eb0ecf74bede977
Justin Morris <desk@pixelbloom.com>
2016-08-15 22:17:54 +1000

    registry path fix

--- .buildkite/steps/release.bash
+++ .buildkite/steps/release.bash
@@ -11,7 +11,7 @@ export MIX_ENV="$MIX_ENV"
 main() {
   pushd "services/nebula" > /dev/null
   echo '--- :elixir: :docker: Building app image...'
-  script/build "nabula_app:${RELEASE_BUILD_VERSION}"
+  script/build "nebula/app:${RELEASE_BUILD_VERSION}"
 
   echo '--- :elixir: :truck: Making release artifact for app...'
   script/release
@@ -19,14 +19,14 @@ main() {
 
   pushd "services/web" > /dev/null
   echo '--- :nginx: :docker: Building web image...'
-  script/build "nabula_web:${RELEASE_BUILD_VERSION}"
+  script/build "nebula/web:${RELEASE_BUILD_VERSION}"
   popd > /dev/null
 
   echo '--- :crystal_ball: :docker: Pushing built images...'
-  docker tag "nabula_web:${RELEASE_BUILD_VERSION}" "${REGISTRY_PATH}/nabula_web:${RELEASE_BUILD_VERSION}"
-  docker tag "nabula_app:${RELEASE_BUILD_VERSION}" "${REGISTRY_PATH}/nabula_app:${RELEASE_BUILD_VERSION}"
-  docker push "${REGISTRY_PATH}/nabula_web:${RELEASE_BUILD_VERSION}"
-  docker push "${REGISTRY_PATH}/nabula_app:${RELEASE_BUILD_VERSION}"
+  docker tag "nebula/web:${RELEASE_BUILD_VERSION}" "${REGISTRY_PATH}/nebula/web:${RELEASE_BUILD_VERSION}"
+  docker tag "nebula/app:${RELEASE_BUILD_VERSION}" "${REGISTRY_PATH}/nebula/app:${RELEASE_BUILD_VERSION}"
+  docker push "${REGISTRY_PATH}/nebula/web:${RELEASE_BUILD_VERSION}"
+  docker push "${REGISTRY_PATH}/nebula/app:${RELEASE_BUILD_VERSION}"
 }
 
 main
