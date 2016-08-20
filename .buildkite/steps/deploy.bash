5eee3889e320b2aa13695dfcec411dda55e44e15
Justin Morris <desk@pixelbloom.com>
2016-08-21 00:17:53 +1000

    gcloud info

--- .buildkite/steps/deploy.bash
+++ .buildkite/steps/deploy.bash
@@ -8,6 +8,7 @@ IFS=$'\n\t'
 readonly DEPLOY_MACHINE_NAME="${DEPLOY_MACHINE_NAME}"
 
 main() {
+  echo '--- :rocket: Deploying...'
   gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --command="ls -al"
 }
 
