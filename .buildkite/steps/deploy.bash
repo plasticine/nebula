5abda0e17dd019934b470906912a4737ca5aed4d
Justin Morris <desk@pixelbloom.com>
2016-08-21 00:21:25 +1000

    Set region and zone

--- .buildkite/steps/deploy.bash
+++ .buildkite/steps/deploy.bash
@@ -9,6 +9,8 @@ readonly DEPLOY_MACHINE_NAME="${DEPLOY_MACHINE_NAME}"
 
 main() {
   echo '--- :rocket: Deploying...'
+  gcloud config set compute/region us-central1
+  gcloud config set compute/zone us-central1-a
   gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --command="ls -al"
 }
 
