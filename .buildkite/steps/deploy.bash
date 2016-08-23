fa1ae37bfdb7653d22c2559b66366cf90a87eddf
Justin Morris <desk@pixelbloom.com>
2016-08-24 06:39:26 +1000

    Remove debugging stuff.

--- .buildkite/steps/deploy.bash
+++ .buildkite/steps/deploy.bash
@@ -12,8 +12,6 @@ main() {
   gcloud config set compute/region us-central1
   gcloud config set compute/zone us-central1-a
   gcloud compute copy-files --quiet job.nomad "${DEPLOY_MACHINE_NAME}:~/job.nomad"
-  gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --quiet --command="ls -al"
-  gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --quiet --command="cat ~/job.nomad"
   gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --quiet --command="nomad status"
   gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --quiet --command="nomad run ~/job.nomad"
 }
