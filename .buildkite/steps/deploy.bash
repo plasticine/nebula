97a5e087901d09f91940182a520fe8dbefb87a1c
Justin Morris <desk@pixelbloom.com>
2016-08-23 22:49:05 +1000

    Try and actually run the deploy.

--- .buildkite/steps/deploy.bash
+++ .buildkite/steps/deploy.bash
@@ -11,7 +11,11 @@ main() {
   echo '--- :rocket: Deploying...'
   gcloud config set compute/region us-central1
   gcloud config set compute/zone us-central1-a
+  gcloud compute copy-files "${DEPLOY_MACHINE_NAME}" --quiet job.nomad deploy/job.nomad
   gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --quiet --command="ls -al"
+  gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --quiet --command="cat deploy/job.nomad"
+  gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --quiet --command="nomad status"
+  gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --quiet --command="nomad run deploy/job.nomad"
 }
 
 main
