0b61b460dd0ce8bb6005bce3777b057fbc607819
Justin Morris <desk@pixelbloom.com>
2016-08-24 09:51:16 +1000

    Cleanup, actual command.

--- .buildkite/steps/deploy.bash
+++ .buildkite/steps/deploy.bash
@@ -7,13 +7,18 @@ IFS=$'\n\t'
 
 readonly DEPLOY_MACHINE_NAME="${DEPLOY_MACHINE_NAME}"
 
+readonly REMOTE_COMMAND=$(cat <<EOF
+export NOMAD_ADDR="http://10.128.0.4:4646"
+nomad status
+EOF
+)
+
 main() {
   echo '--- :rocket: Deploying...'
   gcloud config set compute/region us-central1
   gcloud config set compute/zone us-central1-a
   gcloud compute copy-files --quiet job.nomad "${DEPLOY_MACHINE_NAME}:~/job.nomad"
-  gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --quiet --command="nomad status"
-  gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --quiet --command="nomad run ~/job.nomad"
+  gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --quiet --command="${REMOTE_COMMAND}"
 }
 
 main
