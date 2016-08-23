37dbbd320607f011a65ec078de98538bb524d4fe
Justin Morris <desk@pixelbloom.com>
2016-08-23 23:00:52 +1000

    Use actual path.

--- .buildkite/steps/deploy.bash
+++ .buildkite/steps/deploy.bash
@@ -11,11 +11,11 @@ main() {
   echo '--- :rocket: Deploying...'
   gcloud config set compute/region us-central1
   gcloud config set compute/zone us-central1-a
-  gcloud compute copy-files --quiet job.nomad "${DEPLOY_MACHINE_NAME}:deploy/job.nomad"
+  gcloud compute copy-files --quiet job.nomad "${DEPLOY_MACHINE_NAME}:~/job.nomad"
   gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --quiet --command="ls -al"
-  gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --quiet --command="cat deploy/job.nomad"
+  gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --quiet --command="cat ~/job.nomad"
   gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --quiet --command="nomad status"
-  gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --quiet --command="nomad run deploy/job.nomad"
+  gcloud compute ssh "${DEPLOY_MACHINE_NAME}" --quiet --command="nomad run ~/job.nomad"
 }
 
 main
