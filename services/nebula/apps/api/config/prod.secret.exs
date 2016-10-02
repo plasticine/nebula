9817678aa4671f819805a3f0b8994f1a6526e8fa
Justin Morris <desk@pixelbloom.com>
2016-10-02 20:32:24 +1100

    Prod config

---
+++ services/nebula/apps/api/config/prod.secret.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or you later on).
+config :api, Nebula.Api.Endpoint,
+  secret_key_base: "Q6ufet+pEpJZqGno/emxbuu64Np8pPu4cI81oAuQ8ok1lQ5ymQ8MMCMbtABCRd7E"
